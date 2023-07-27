53036223c5980614c5ea371648e6bf29269b8ffe
pattaraponpetch <pattaraponyainontad16520@gmail.com>
2023-07-27 09:42:56 +0700

    TEST00

--- ex02.js
+++ ex02.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const knex = require('knex')
 const app = express()
 const multer = require('multer')
+const { raw } = require('mysql')
 
 const db = knex({
   client: 'mysql',
@@ -22,18 +23,114 @@ const db = knex({
 
 app.use(cors())
 app.use(bodyParser.json())
+// http://localhost:7001
 app.get('/', (req, res) => {
+  console.log('test api')
   res.send({ ok: 1 })
 })
+// http://localhost:7001/lists
 app.get('/lists', async (req, res) => {
-  console.log('lists')
+  console.log('show user')
   let row = await db('users_advisor')
+  // .where("major_id", 98)
   res.send({
     datas: row,
     status: 1,
   })
 })
+app.get('/list', async (req, res) => {
+  try{
+     console.log('show user')
+  let row = await db('member')
+  // .where("major_id", 98)
+  res.send({
+    datas: row,
+    status: 1,
+  })
+  }catch (e) {
+    console.log(e.message)
+    res.send({
+      ok: 0,
+      error: e.message
+    })
+  }
+})
 
+app.get('/update',async (req,res)=>{
+  try{
+    console.log(req.query)
+    let row = await db('member')
+    .where('id','=', req.query.id)
+    .update({
+      username : req.query.username,
+      password : req.query.password,
+      dep : req.query.dep
+    })
+    console.log('row=',row)
+    res.send({
+      ok : 1,
+      data : row
+  })
+}catch (e) {
+  console.log(e.message)
+  res.send({
+    ok: 0,
+    error: e.message
+  })
+}
+})
+
+app.get('/delete', async (req, res) => {
+  try{
+    console.log('show id=', req.query)
+  let row = await db('member').where('id', req.query.id)
+  .where('id','=', req.query.id)
+  .del()  
+  res.send({
+    datas: row[0],
+    status: 1,
+  })
+  } catch (e) {
+    console.log(e.message)
+    res.send({
+      ok: 0,
+      error: e.message
+    })
+  }
+})
+
+app.get('/listedit', async (req, res) => {
+  console.log('show id', req.query)
+  let row = await db('member').where('id', req.query.id)
+  res.send({
+    datas: row[0],
+    status: 1,
+  })
+})
+
+
+app.get('/insert', async (req, res) => {
+  try {
+    console.log(req.query)
+    console.log(req.query.name)
+    ids = await db('member').insert({
+      username: req.query.name,
+      password: req.query.password,
+      dep: req.query.dep
+    })
+    res.send({
+      ok: 1,
+      data: req.query,
+      ids: ids[0]
+    })
+  } catch (e) {
+    console.log(e.message)
+    res.send({
+      ok: 0,
+      error: e.message
+    })
+  }
+})// insert
 app.listen(7001, () => {
   console.log('ready:candle:7001')
 })
